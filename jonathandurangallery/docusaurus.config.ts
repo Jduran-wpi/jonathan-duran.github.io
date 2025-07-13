0c2df198840a051b73c07ff2229830235851bfd5
Jduran-wpi <87102538+Jduran-wpi@users.noreply.github.com>
2025-07-12 23:12:32 -0400

    second commit

--- jonathandurangallery/docusaurus.config.ts
+++ jonathandurangallery/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://jonathan-duran.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -24,6 +24,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'jduran-wpi', // Usually your GitHub org/user name.
   projectName: 'personal-website', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
